52837cc3be97d7dfa8402014cc7e400e056d4408
Jose Damico <jose.damico@scicrop.com>
2023-12-08 15:04:32 -0300

    improved telnet client

--- jbasigate/src/main/java/org/jdamico/jbasigate/App.java
+++ jbasigate/src/main/java/org/jdamico/jbasigate/App.java
@@ -6,85 +6,61 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
 
-import org.jdamico.javax25.ax25.Packet;
+import org.jdamico.javax25.ax25.Afsk1200Demodulator;
 
 public class App {
+	
     public static void main(String[] args){
         String host = args[2];
         int port = Integer.parseInt(args[3]);
         String callSign = args[0].toUpperCase();
         String passcode = args[1];
-        SocketClient sc =  new SocketClient();
-        
+        Double lat = Double.parseDouble(args[4]);
+        Double lng = Double.parseDouble(args[5]);
+        Long lastVisitTime = 0l;
         AprsHelper aprsHelper = new AprsHelper();
-        String msg = aprsHelper.getDateTimeLocMsg(new Date(), -3, -23.5429886,-46.7575315);
-        
-		Packet packet = new Packet("APRS",
-				callSign,
-				new String[] {},
-				Packet.AX25_CONTROL_APRS,
-				Packet.AX25_PROTOCOL_NO_LAYER_3,
-				msg.getBytes());
-		
-		System.out.println(packet.toString());
         boolean is_logged = false;
         try (
-                Socket kkSocket = new Socket(host, port);
-                PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+                Socket igateSocket = new Socket(host, port);
+                PrintWriter out = new PrintWriter(igateSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(
-                    new InputStreamReader(kkSocket.getInputStream()));
+                    new InputStreamReader(igateSocket.getInputStream()));
             ) {
-                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
                 String fromServer;
-                String fromUser;
+                
      
                 while ((fromServer = in.readLine()) != null) {
                     System.out.println("Server: " + fromServer);
-                     
-                    //fromUser = stdIn.readLine();
                     if(!is_logged) {
                     	out.println("user "+callSign+" pass "+passcode+" vers jBasIGATE 0.0.1");
-                    	System.out.println("PU2LVM-10>APRS:"+aprsHelper.getLocMsg(-23.5429886,-46.7575315, "jBasIGATE https://github.com/damico/jBasIGATE"));
-                    	String packetMsg = "PU2LVM-10>APRS,TCPIP*:>https://github.com/damico/jBasIGATE";
+                    	String packetMsg = callSign+">APRS,TCPIP*:>https://github.com/damico/jBasIGATE";
                     	out.println(packetMsg);
-                    	out.println("PU2LVM-10>APRS,TCPIP*:"+aprsHelper.getLocMsg(-23.5429886,-46.7575315, "jBasIGATE\r"));
-                    	
+                    	packetMsg = callSign+">APRS:"+aprsHelper.getLocMsg(lat,lng, "jBasIGATE https://github.com/damico/jBasIGATE");
+                    	out.println(packetMsg);
                     	is_logged = true;
+                    }else {
+                    	if(Afsk1200Demodulator.receivedPackedMap != null && Afsk1200Demodulator.receivedPackedMap.size() > 0) {
+                    		Set<Long> packetsReceived = Afsk1200Demodulator.receivedPackedMap.keySet();
+                    		Iterator<Long> packetsReceivedIter = packetsReceived.iterator();
+                    		while(packetsReceivedIter.hasNext()) {
+                    			Long packetsReceivedTime = packetsReceivedIter.next();
+                    			if(packetsReceivedTime > lastVisitTime) {
+                    				lastVisitTime = packetsReceivedTime;
+                    				out.println(callSign+">"+Afsk1200Demodulator.receivedPackedMap.get(packetsReceivedTime));
+                    			}
+                    		}
+                    	}
                     }
 
                 }
             } catch (UnknownHostException e) {
                 System.err.println("Don't know about host " + host);
-                System.exit(1);
             } catch (IOException e) {
                 System.err.println("Couldn't get I/O for the connection to " +
                     host);
-                System.exit(1);
             }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//        try {
-//			sc.startConnection(host, port);
-//			sc.sendMessage("user "+callSign+" pass "+passcode+" vers jBasIGATE 0.0.1\r");
-//			sc.sendMessage(callSign+">APRS,TCPIP*:>This is a test packet from jBasIGATE\r");
-//			
-//		} catch (UnknownHostException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
     }
 }
